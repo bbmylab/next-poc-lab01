525a0b21f78399bdd331ac7801b4cd7f35220fbc
BbmyDevTeam <office.bbmy@gmail.com>
2022-03-30 13:07:22 +0700

    Update Blank template

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,17 +7,13 @@ function Hero() {
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='/images/site/max.png'
+          src='/images/site/nextlogo.png'
           alt='An image showing Max'
           width={300}
           height={300}
         />
       </div>
-      <h1>Hi, I'm Max</h1>
-      <p>
-        I blog about web development - especially frontend frameworks like
-        Angular or React.
-      </p>
+      <h1>Hi, NEXTJS</h1>
     </section>
   );
 }
